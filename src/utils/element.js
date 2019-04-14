b09476b8168b5c83989fcaac21342370838b8862
Tin Nguyen <tinnguyen263@outlook.com>
2019-04-14 13:34:23 +0700

    add eslint, refactor all, remove unused files

--- src/utils/element.js
+++ src/utils/element.js
@@ -1,20 +1,21 @@
-const removePx = value => (typeof value === "string" && value.endsWith('px')) ? +value.substring(0, value.length - 2) : +value;
+const removePx = (value) => (typeof value === 'string' &&
+    value.endsWith('px')) ? +value.substring(0, value.length - 2) : +value;
 
-export const getPosition = element => {
+export const getPosition = (element) => {
   return {
     top: removePx(element.offsetTop),
-    left: removePx(element.offsetLeft)
-  }
+    left: removePx(element.offsetLeft),
+  };
 };
 
-export const getSize = element => {
+export const getSize = (element) => {
   const styles = window.getComputedStyle(element);
   const width = styles.width;
   const height = styles.height;
   return {
     width: removePx(width),
-    height: removePx(height)
-  }
+    height: removePx(height),
+  };
 };
 
 export const moveElement = (element, top, left) => {
