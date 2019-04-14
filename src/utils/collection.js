8eac42e12096b1235c4d815419f173a759cd8489
Tin Nguyen <tinnguyen263@outlook.com>
2019-04-14 11:43:08 +0700

    refactor collection util

---
+++ src/utils/collection.js
@@ -0,0 +1,14 @@
+const ArrayCollection = {
+  getAllItems: collection => [...collection],
+  addItem: (collection, item) => [...collection, item],
+  updateItem: (collection, item) => collection.map(i => i.id === item.id ? item : i),
+  deleteItem: (collection, item) => collection.reduce((newCollection, i) => [...newCollection, ...(i.id === item.id ? [] : [i])], []),
+  of : items => ({
+    getAllItems: items => ArrayCollection.getAllItems(items),
+    addItem: (item) => ArrayCollection.addItem(items, item),
+    updateItem: (item) => ArrayCollection.updateItem(items, item),
+    deleteItem: (item) => ArrayCollection.deleteItem(items, item)
+  })
+};
+
+export { ArrayCollection }
